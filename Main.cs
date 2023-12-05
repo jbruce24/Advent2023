4edf4fa7e287788177f535d8fcc9155ccb73e7aa
jbruce-doaneu <jared.bruce@doane.edu>
2023-12-05 09:33:40 -0500

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent
+{
+    public class Start
+    {
+        public static void Main()
+        {
+            Day1.Run();
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Advent
+{ 
+    public class Day1
+    {
+        public static void Run()
+        {
+            string line;
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\Jared\\sourceP\\repos\\AdventOfCode23\\Files\\day1.txt");
+                StreamWriter sw = new StreamWriter("C:\\Users\\Jared\\sourceP\\repos\\AdventOfCode23\\Files\\day1Output.txt");
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    sw.WriteLine(line + "\n");
+                    sw.Close();
+                    line = sr.ReadLine();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Hello 2");
+            }
+        }
+    }
+}
